00bbd113553492128c0293289b887fed5234f7de
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-10-21 20:09:43 +0530

    :wrench: removed deprecated code and geocoder

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ var express     = require("express"),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
     passport    = require("passport"),
-    LocalStrategy=require("passport-local"),
-    methodOverride = require("method-override"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"), // using to support PUT method.
     passportLocalMongoose = require("passport-local-mongoose"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
-    flash =require("connect-flash"),
     User        = require("./models/user"),
     seedDB      = require("./seeds");
 
@@ -17,13 +16,13 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 // mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://AdithyaBhat:Rusty@ds111476.mlab.com:11476/yelpcamp_adithya", { useNewUrlParser: true });
+mongoose.connect("mongodb://AdithyaBhat:Rusty@ds111476.mlab.com:11476/yelpcamp_adithya", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(flash());
-// seedDB();
+app.use(bodyParser.json());
+// seedDB(); // use this to fill your db with default data.
 
 //passport config
 
@@ -41,8 +40,6 @@ app.use(methodOverride("_method"));
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
     next();
 });
 
