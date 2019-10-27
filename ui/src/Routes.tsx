16bb4d2477d308b967249301a34e598a2fac63f4
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-10-27 17:06:40 +0530

    :sparkles: route for adding campgrounds

--- ui/src/Routes.tsx
+++ ui/src/Routes.tsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import App from './components/App'
 import Campground from './components/CampgroundPage'
 import Auth from './components/Auth'
+import AddCampground from './components/AddCampground'
 
 // @todo create username context 
 export default () => {
@@ -13,7 +14,7 @@ export default () => {
                 <Route exact path="/" component={App}/>
                 <Route exact path="/login" render={(props) => <Auth purpose='login' {...props}/>}/>
                 <Route exact path="/signup" render={(props) => <Auth purpose='signup' {...props}/>}/>
-                <Route exact path="/add" render={(props) => <div>add</div>}/>
+                <Route exact path="/add" render={(props) => <AddCampground {...props}/>}/>
                 <Route exact path="/:id" component={Campground}/>
             </Switch>
         </Router>
