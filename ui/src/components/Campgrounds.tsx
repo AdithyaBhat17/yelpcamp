ed33ea2e66c98d9b25c27a7dd4c8c818be513b2a
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-11-06 23:16:25 +0530

    :sparkles: fix: show empty illustration

--- ui/src/components/Campgrounds.tsx
+++ ui/src/components/Campgrounds.tsx
@@ -3,6 +3,7 @@ import { useFetch } from '../hooks/useFetch'
 import { Link } from 'react-router-dom'
 import { IntersectingCirclesSpinner } from 'react-epic-spinners'
 import empty from '../assets/empty.png'
+import { JSXElement } from '@babel/types'
 
 interface Campground {
     _id: string;
@@ -20,25 +21,37 @@ const Campgrounds: React.FC<Props> = ({search}) => {
     
     if(loading || (data && !data.campgrounds))
         return <IntersectingCirclesSpinner color="#feca76" className="loading"/>
+
+    const renderCampgrounds = (campgrounds: Array<Campground>, search: string) => {
+        let campgroundsJSX: Array<JSX.Element> = []
+        for(let camp = 0; camp < campgrounds.length; camp++) {
+            if(campgrounds[camp].name.toLowerCase().includes(search))
+                campgroundsJSX.push(
+                    <div key={campgrounds[camp]._id} className="col-lg-3 col-md-6 col-sm-12">
+                        <div className="thumbnail campground-cards">
+                            <img className="thumbnail-img" src={campgrounds[camp].image} alt={campgrounds[camp].name}/>
+                            <p className="card-p">{campgrounds[camp].name}</p>
+                            <p className="card-p">
+                                <Link to={`/${campgrounds[camp]._id}`}>Learn More</Link>
+                            </p>
+                        </div>
+                    </div>
+                )
+        }
+        if(campgroundsJSX.length)
+            return campgroundsJSX
+        else
+            return (
+                <div className="empty">
+                    <img src={empty} alt="no campgrounds available"/>
+                </div>
+            )
+    }
     
     return (
         <div className="container">
             <div className="row">
-                {data && data.campgrounds.map((campground: Campground, index: Number) => 
-                campground.name.toLowerCase().includes(search) ? (
-                    <div key={campground._id} className="col-lg-3 col-md-6 col-sm-12">
-                        <div className="thumbnail campground-cards">
-                            <img className="thumbnail-img" src={campground.image} alt={campground.name}/>
-                            <p className="card-p">{campground.name}</p>
-                            <p className="card-p">
-                                <Link to={`/${campground._id}`}>Learn More</Link>
-                            </p>
-                        </div>
-                    </div>
-                ) : index === 0 &&
-                <div key={campground._id} className="empty">
-                    <img src={empty} alt="no campgrounds available"/>
-                </div>)}
+                {data && renderCampgrounds(data.campgrounds, search)}
             </div>
         </div>
     )
