5933897cd8fb55cc00e835b56bfd3c902e985796
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-11-12 22:23:13 +0530

    :sparkles: added headers to fix username not given error

--- ui/src/components/Auth/index.tsx
+++ ui/src/components/Auth/index.tsx
@@ -19,9 +19,12 @@ const Auth: React.FC<Props> = ({purpose, history, location}) => {
             setError('Username / Password is empty :(')
             return
         }
-        fetch(`${process.env.REACT_APP_BASE_URL}/${purpose}/`, {
+        fetch(`${process.env.REACT_APP_BASE_URL}/${purpose}`, {
             method: 'POST',
             credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: JSON.stringify({username, password})
         })
         .then(res => res.json())
