951f558f990b1fd3c79a3f16d7ddcde5e548b020
Siddharth Subramanian <sidsbrmnn@gmail.com>
2020-01-04 00:57:14 +0530

    Refactored comments route

--- routes/comments.js
+++ routes/comments.js
@@ -1,73 +1,75 @@
-var express = require("express");
-var router  = express.Router({mergeParams: true});
-var Campground = require("../models/campground");
-var Comment = require("../models/comment");
+const express = require('express');
+const mongoose = require('mongoose');
 
-//Comments Create
-// @todo send added comment as response instead of redirecting to campgrounds view
-router.post("/",isLoggedIn,function(req, res){
-   //lookup campground using ID
-   Campground.findById(req.params.id, function(err, campground){
-       if(err){
-           console.log(err);
-           res.send({success: false, err});
-       } else {
-            let comment = {text: req.body.comment, author: req.user.username}
-            Comment.create(comment, function(err, comment){
-            if(err){
-                console.log(err);
-                res.send({success: false, err});
-            } else {
-                campground.comments.push(comment);
-                campground.save();
-                res.send({success: true, comment});
-            }
-        });
-       }
-   });
-});
+const { isLoggedIn, isValidId } = require('../middlewares');
+const Comment = require('../models/comment');
+const Campground = require('../models/campground');
 
-router.delete("/:commentId",isLoggedIn,function(req, res){
-    Comment.findById(req.params.commentId, function(err, comment){
-        if(err){
-            res.send({success: false, err});
-        }else{
-            if(comment.author === req.user.username) {
-                Comment.deleteOne({_id: req.params.commentId}, function(err) {
-                    if(err) {
-                        res.status(400).send({
-                            success: false,
-                            err
-                        })
-                    } else {
-                        Campground.find({}, function(_err, campgrounds) {
-                          campgrounds.forEach(campground => {
-                            let index = campground.comments.indexOf(req.params.commentId);
-                            if(index !== -1)
-                              campground.comments.splice(index, 1)
-                              campground.save()
-                          })
-                        })
-                        res.status(200).send({success: true, message: 'Successfully deleted campground!'});
-                    }
-                })
-            } else {                
-                res.status(401).send({
-                    success: false,
-                    message: 'You can only delete your own comments :/'
-                })
-            }
-        }
-    });
-});
+const router = express.Router();
 
-//middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
+// Add a comment
+router.post('/', [isLoggedIn, isValidId], async (req, res) => {
+  try {
+    const campground = await Campground.findOne({ _id: req.params.id });
+    if (!campground) {
+      return res
+        .status(400)
+        .send({ success: false, message: 'Invalid campground' });
     }
-    res.send({success: false, isLoggedIn: false, message: 'Please log in to enjoy our services!'});
-}
 
+    const comment = new Comment({
+      text: req.body.comment,
+      author: req.user.username
+    });
+    campground.comments.push(comment._id);
+    await Promise.all([comment.save(), campground.save()]);
+
+    res.send({ success: true, comment });
+  } catch (err) {
+    res.send(500).send({ success: false, err });
+  }
+});
+
+// Delete a comment
+router.delete('/:commentId', isLoggedIn, async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.commentId)) {
+      return res.status(400).send({ success: false, message: 'Invalid ID' });
+    }
+
+    const campground = await Campground.findOne({ _id: req.params.id });
+    if (!campground) {
+      return res
+        .status(400)
+        .send({ success: false, message: 'Invalid campground' });
+    }
+
+    const comment = await Comment.findOne({ _id: req.params.commentId });
+    if (!comment) {
+      return res
+        .status(404)
+        .send({ success: false, message: 'Comment not found' });
+    }
+
+    if (comment.author !== req.user.username) {
+      return res.status(403).send({
+        success: false,
+        message: 'You can only delete your own comments'
+      });
+    }
+
+    const index = campground.comments.indexOf(comment._id);
+    if (index > -1) {
+      campground.comments.splice(index, 1);
+      await Promise.all([comment.remove(), campground.save()]);
+    } else {
+      await comment.remove();
+    }
+
+    res.send({ success: true, message: 'Successfully deleted comment' });
+  } catch (err) {
+    res.status(500).send({ success: false, err });
+  }
+});
 
 module.exports = router;
